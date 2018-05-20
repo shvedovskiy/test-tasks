b3cfa2b4682e5dc72684f2f5f2c654e9e06b59ec
Oleg Shvedov <ol.shvedov@gmail.com>
2018-05-20 21:15:52 +0300

    Add app-specific components (structured template)

--- aviasales/solution/src/index.js
+++ aviasales/solution/src/index.js
@@ -1,26 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
 
-import App from '~/components/app';
-import configureStore from '~/configure-store';
+import Search from '~/components/search';
 
-
-const store = configureStore();
 
 render(
-  <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
-  </Provider>,
+  <Search />,
   document.getElementById('app')
 );
 
 if (module.hot) {
   module.hot.accept();
-  module.hot.accept('./reducers', () => {
-    store.replaceReducer(require('./reducers').default);
-  });
 }
