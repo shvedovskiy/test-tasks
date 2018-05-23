2cf0ed9d33a3c4fe12431eb6b921d714eefa186a
Oleg Shvedov <ol.shvedov@gmail.com>
2018-05-23 19:14:49 +0300

    Unify development and production webpack configs code style

--- aviasales/solution/config/webpack.config.dev.js
+++ aviasales/solution/config/webpack.config.dev.js
@@ -3,36 +3,39 @@ const webpack = require('webpack');
 const autoprefixer = require('autoprefixer');
 const DirectoryNamedWebpackPlugin = require('directory-named-webpack-plugin');
 
-const srcPath = path.join(__dirname, '..', 'src');
 
+const srcPath = path.join(__dirname, '..', 'src');
 
 module.exports = {
 	mode: 'development',
 	context: srcPath,
 	entry: [
     'webpack-hot-middleware/client',
-		'./index.js'
+		'./index.js',
 	],
 	output: {
 		path: srcPath,
 		filename: 'bundle.js',
-		publicPath: '/static'
+		publicPath: '/static/',
 	},
 	resolve: {
-        modules: ['node_modules', 'src'],
-        alias: {
-            '~': srcPath
-        },
-        plugins: [
-            new DirectoryNamedWebpackPlugin(true)
-        ]
+    modules: [
+      'node_modules',
+      'src',
+    ],
+    alias: {
+      '~': srcPath,
     },
+    plugins: [
+      new DirectoryNamedWebpackPlugin(true),
+    ],
+  },
 	module: {
 		rules: [
 			{
 				test: /\.js$/,
+        exclude: /node_modules/,
 				loader: 'babel-loader',
-				exclude: /node_modules/
 			},
 			{
         test: /\.css$/,
@@ -42,30 +45,35 @@ module.exports = {
             loader: 'css-loader',
             options: {
               sourceMap: true,
-              importLoaders: 1
-            }
+              importLoaders: 1,
+            },
           },
           {
             loader: 'postcss-loader',
             options: {
-              plugins: [autoprefixer()]
-            }
-          }
-        ]
+              plugins: [autoprefixer()],
+            },
+          },
+        ],
       },
       {
         test: [/\.svg$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
         loader: 'file-loader',
         options: {
           name: 'media/[name].[ext]',
-          publicPath: '/static'
-        }
-      }
-		]
+          publicPath: '/static/',
+        },
+      },
+		],
 	},
 	plugins: [
-		new webpack.HotModuleReplacementPlugin()
+		new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      },
+    }),
 	],
 	target: 'web',
-  devtool: 'source-map'
+  devtool: 'source-map',
 };
--- aviasales/solution/config/webpack.config.prod.js
+++ aviasales/solution/config/webpack.config.prod.js
@@ -1,88 +1,101 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const webpack = require('webpack');
 const autoprefixer = require('autoprefixer');
 const DirectoryNamedWebpackPlugin = require('directory-named-webpack-plugin');
 const rimraf = require('rimraf');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
 
 const srcPath = path.join(__dirname, '..', 'src');
 const outputPath = path.join(__dirname, '..', 'dist');
 
-
 module.exports = {
-    mode: 'production',
-    context: srcPath,
-    entry: './index.js',
-    output: {
-        path: outputPath,
-        filename: 'bundle.js',
-        publicPath: '/static/'
-    },
-    resolve: {
-        modules: ['node_modules', 'src'],
-        alias: {
-            '~': path.join(srcPath)
-        },
-        plugins: [
-            new DirectoryNamedWebpackPlugin(true)
-        ]
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            importLoaders: 1
-                        }
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            plugins: [autoprefixer()]
-                        }
-                    }
-                ]
-            },
-            {
-                test: [/\.svg$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-                loader: 'file-loader',
-                options: {
-                    name: 'media/[name].[ext]',
-                    publicPath: '/static/'
-                }
-            }
-        ]
-    },
-    optimization: {
-        minimizer: [
-            new UglifyJsPlugin({
-                cache: true,
-                parallel: true
-            }),
-            new OptimizeCSSAssetsPlugin()
-        ]
+  mode: 'production',
+  context: srcPath,
+  entry: [
+    './index.js',
+  ],
+  output: {
+    path: outputPath,
+    filename: 'bundle.js',
+    publicPath: '/static/',
+  },
+  resolve: {
+    modules: [
+      'node_modules',
+      'src',
+    ],
+    alias: {
+      '~': srcPath,
     },
     plugins: [
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-            chunkFilename: '[id].css'
-        }),
-        () => {
-            console.info('Clearing /dist directory');
-            rimraf.sync(outputPath, require('fs'), err => {
-                if (err) console.error('Clearing of /dist directory failed', err);
-            });
-        }
+      new DirectoryNamedWebpackPlugin(true),
     ],
-    target: 'web'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [autoprefixer()],
+            },
+          },
+        ],
+      },
+      {
+        test: [/\.svg$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+        loader: 'file-loader',
+        options: {
+          name: 'media/[name].[ext]',
+          publicPath: '/static/',
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+      }),
+      new OptimizeCSSAssetsPlugin(),
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      },
+    }),
+    () => {
+      console.info('Clearing /dist directory');
+      rimraf.sync(outputPath, require('fs'), err => {
+        if (err) {
+          console.error('Clearing of /dist directory failed', err);
+        }
+      });
+    },
+  ],
+  target: 'web',
 };
