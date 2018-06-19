772852d5a671862fb4ac627db909d11b267ea989
Oleg Shvedov <ol.shvedov@gmail.com>
2018-06-19 12:41:04 +0300

    Delete env variables assignment by destructuring due to specificity of dotenv webpack plugin

--- aviasales/solution/src/client/config/config.js
+++ aviasales/solution/src/client/config/config.js
@@ -1,10 +1,8 @@
-export const {
-  NODE_ENV,
-  SERVER_HOSTNAME,
-  SERVER_PORT,
-  DEV_SERVER_PORT,
-  HTTPS,
-  FIXER_API_KEY,
-} = process.env;
+export const NODE_ENV = process.env.NODE_ENV;
+export const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME;
+export const SERVER_PORT = process.env.SERVER_PORT;
+export const DEV_SERVER_PORT = process.env.DEV_SERVER_PORT;
+export const HTTPS = process.env.HTTPS;
+export const FIXER_API_KEY = process.env.FIXER_API_KEY;
 
 export const APP_COMPONENT_SELECTOR = 'app';
