f92ab8d4fd43316e45fea9313f5fe2619fd60abb
Oleg Shvedov <ol.shvedov@gmail.com>
2018-06-20 19:51:07 +0300

    Delete several boilerplate action creator functions

--- aviasales/solution/src/client/store/tickets/actions.js
+++ aviasales/solution/src/client/store/tickets/actions.js
@@ -5,29 +5,16 @@ import ticketService from '~/services/tickets';
 import { getIsFetching } from '~/store/rootSelectors';
 import { setStopsFilter } from '~/store/settings/actions';
 /* eslint-enable import/extensions, import/no-unresolved */
-
 import * as types from './action-types';
 
-
-const fetchTicketsRequest = () => ({
-  type: types.FETCH_TICKETS_REQUEST,
-});
-
-const fetchTicketsSuccess = (tickets, ids) => ({
-  type: types.FETCH_TICKETS_SUCCESS,
-  payload: { tickets, ids },
-});
-
-const fetchTicketsFailure = errorMessage => ({
-  type: types.FETCH_TICKETS_FAILURE,
-  payload: { errorMessage },
-});
 
 export const fetchTickets = () =>
   async (dispatch, getState) => {
     if (getIsFetching(getState()) === false) {
       dispatch(setStopsFilter());
-      dispatch(fetchTicketsRequest());
+      dispatch({
+        type: types.FETCH_TICKETS_REQUEST,
+      });
 
       try {
         const tickets = await ticketService.getTickets();
@@ -36,9 +23,15 @@ export const fetchTickets = () =>
         const filter = _.uniq(_.map(ticketsById, 'stops'));
 
         dispatch(setStopsFilter(...filter));
-        dispatch(fetchTicketsSuccess(ticketsById, ids));
-      } catch (error) {
-        dispatch(fetchTicketsFailure(error));
+        dispatch({
+          type: types.FETCH_TICKETS_SUCCESS,
+          payload: { tickets: ticketsById, ids },
+        });
+      } catch (errorMessage) {
+        dispatch({
+          type: types.FETCH_TICKETS_FAILURE,
+          payload: { errorMessage },
+        });
       }
     }
   };
