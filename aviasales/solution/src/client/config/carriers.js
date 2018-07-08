3b6add6007e01ae33024213195beea68e1325b6d
Oleg Shvedov <ol.shvedov@gmail.com>
2018-07-08 14:10:42 +0300

    Configure paths to carrier logo images

--- aviasales/solution/src/client/config/carriers.js
+++ aviasales/solution/src/client/config/carriers.js
@@ -1,8 +1,13 @@
+import { STATIC_PATH } from './config';
+
+
+const PREFIX = `${STATIC_PATH}images/carriers/`;
+
 /* eslint-disable import/prefer-default-export */
 export const carrierLogos = {
-  TK: 'TK',
-  S7: '',
-  SU: '',
-  BA: '',
+  TK: `${PREFIX}TK.png`,
+  S7: `${PREFIX}S7.png`,
+  SU: `${PREFIX}SU.png`,
+  BA: `${PREFIX}BA.png`,
 };
 /* eslint-enable import/prefer-default-export */
--- aviasales/solution/src/client/config/config.js
+++ aviasales/solution/src/client/config/config.js
@@ -1,5 +1,6 @@
 /* eslint-disable prefer-destructuring */
 export const NODE_ENV = process.env.NODE_ENV;
+export const STATIC_PATH = process.env.STATIC_PATH;
 export const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME;
 export const SERVER_PORT = process.env.SERVER_PORT;
 export const DEV_SERVER_PORT = process.env.DEV_SERVER_PORT;
