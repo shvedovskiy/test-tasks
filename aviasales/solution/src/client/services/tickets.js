fa9ca2371143f33e8076fcdacdebd7f8e9340d78
Oleg Shvedov <oleg.shvedov@genesys.com>
2018-08-09 17:16:05 +0300

    Constrain formatDate method types

--- aviasales/solution/src/client/services/tickets.js
+++ aviasales/solution/src/client/services/tickets.js
@@ -14,7 +14,7 @@ dayjs.locale('ru', {
 class TicketService {
   API_ENDPOINT = address;
 
-  static formatDate(rawDate) {
+  static formatDate(rawDate: string): string {
     return dayjs(rawDate.replace(/^(\d{2})\.(\d{2})\.(.*)$/, '$2.$1.$3')).format('D MMM YYYY, dd');
   }
 
